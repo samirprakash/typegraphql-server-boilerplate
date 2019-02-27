47c07c3f34afc61c39f9b0cc93fd33d2d752796b
Samir Prakash <Samir.Prakash@gmail.com>
2019-02-27 15:29:58 -0600

    Finished tests for login workflow

--- src/modules/user/login/Login.test.ts
+++ src/modules/user/login/Login.test.ts
@@ -1,4 +1,3 @@
-import bcrypt from "bcryptjs";
 import faker from "faker";
 import { Connection } from "typeorm";
 import { User } from "../../../entity/User";
@@ -29,34 +28,28 @@ query Login($data: LoginInput!) {
 }`;
 
 describe("User login process :", () => {
-  it("should be able to get the registered user", async () => {
-    const user = {
-      firstName: faker.name.firstName(),
-      lastName: faker.name.lastName(),
-      email: faker.internet.email(),
-      password: await bcrypt.hash(faker.internet.password(), 12)
-    };
+  const user = {
+    firstName: faker.name.firstName(),
+    lastName: faker.name.lastName(),
+    email: faker.internet.email(),
+    password: faker.internet.password(),
+  };
 
+  it("should be able to get the registered user", async () => {
     await User.create(user).save();
 
     try {
-      const gql = await gCall({
+      await gCall({
         source: loginQuery,
         variableValues: {
           data: {
             email: user.email,
-            password: user.password
-          }
-        }
+            password: user.password,
+          },
+        },
       });
-
-      console.log(gql);
     } catch (err) {
       console.log(err);
     }
-  });
-
-  it("should find the user with the registered email", async () => {
-    // await expect(User.findOne({ where: { email: user!.email } }));
   });
 });
